e59d9e4817d97ce6c27994780b6cf9531ca099d4
Erica Case <ecase@impinj.com>
2017-10-10 11:53:41 -0700

    search method works

--- BinaryTree.py
+++ BinaryTree.py
@@ -23,9 +23,19 @@ class BinaryTree(object):
         print(node.value)
         self.print_in_order(node.right)
 
-    # def insert(self, value):
-    #     if self._root == null:
-    #         self._root = Node(value)
+    def search(self, value, node):
+        # Base case: returns true if the node value matches value
+        # returns false if the node is empty.
+        if node is None or node.value == value:
+            return (node is not None)
+
+        # go to the left if  search value is greater than current_node_value
+        if value < node.value:
+            return self.search(value, node.left)
+
+        # go to the left if current node value is less than or equal to
+        # value_to_find
+        return self.search(value, node.right)
 
 
 class TreeNode(object):
@@ -33,21 +43,29 @@ class TreeNode(object):
         self.value = value
         self.left = left
         self.right = right
-    #
-    # @property
-    # def value(self):
-    #     return (self.__value)
+        #
+        # @property
+        # def value(self):
+        #     return (self.__value)
+
 
 def make_tree():
-    root = TreeNode(value=1)
+    root = TreeNode(value=3)
     root.left = TreeNode(value=2)
-    root.right = TreeNode(value=3)
-    root.left.left = TreeNode(value=4)
-    root.left.right = TreeNode(value=5)
+    root.right = TreeNode(value=4)
+    root.left.left = TreeNode(value=1)
+    root.right.left = TreeNode(value=3.5)
+    root.right.right = TreeNode(value=5)
     return BinaryTree(root)
 
 
 tree = make_tree()
+tree.search(3.5, tree.root)
 # tree.print_pre_order(tree.root)
 # tree.print_post_order(tree.root)
 tree.print_in_order(tree.root)
+
+print(tree.search(5, tree.root))
+print(tree.search(1, tree.root))
+print(tree.search(3.5, tree.root))
+print(tree.search(1.5, tree.root))
