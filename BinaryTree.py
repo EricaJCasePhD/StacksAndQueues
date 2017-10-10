cbceb8aac10aa600bb27e64925d4b7c5fe55b03e
Erica Case <ecase@impinj.com>
2017-10-10 11:11:46 -0700

    recursive Binary print works

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,48 +1,53 @@
 class BinaryTree(object):
-    def __init__(self, value=None):
-        self._root = TreeNode(value)
+    def __init__(self, root=None):
+        self.root = root
 
-    def print_pre_order(self, node = self._root):
+    def print_pre_order(self, node):
         if node == None:
             return
-        print node.value
-        print_pre_order(node.left)
-        print_pre_order(node.right)
+        print(node.value)
+        self.print_pre_order(node.left)
+        self.print_pre_order(node.right)
 
-    def print_post_order(self):
+    def print_post_order(self, node):
         if node == None:
             return
-        print_post_order(node.left)
-        print_post_order(node.right)
+        self.print_post_order(node.left)
+        self.print_post_order(node.right)
         print(node.value)
 
-    def print_in_order(self):
-        if node == None:
+    def print_in_order(self, node):
+        if node is None:
             return
-        print_post_order(node.left)
+        self.print_in_order(node.left)
         print(node.value)
-        print_post_order(node.right)
+        self.print_in_order(node.right)
 
-    def insert(self, value):
-        if self._root == null:
-            self._root = Node(value)
+    # def insert(self, value):
+    #     if self._root == null:
+    #         self._root = Node(value)
+
 
 class TreeNode(object):
-    def init(self, value=None, left=None, right=None):
-        self.__value = value
+    def __init__(self, value=None, left=None, right=None):
+        self.value = value
         self.left = left
         self.right = right
+    #
+    # @property
+    # def value(self):
+    #     return (self.__value)
 
-        @property
-        def value(self):
-            return(value)
-
-def make_tree(self):
-    root = Node(1)
-    root.left = Node(2)
-    root.right = Node(3)
-    root.left.left = Node(4)
-    root.left.right = Node(5)
+def make_tree():
+    root = TreeNode(value=1)
+    root.left = TreeNode(value=2)
+    root.right = TreeNode(value=3)
+    root.left.left = TreeNode(value=4)
+    root.left.right = TreeNode(value=5)
     return BinaryTree(root)
 
 
+tree = make_tree()
+# tree.print_pre_order(tree.root)
+# tree.print_post_order(tree.root)
+tree.print_in_order(tree.root)
